9584f4038c410ada63514b1890d64466664b43ca
¨Willcode123¨ <willgonz5.0@gmail.com>
2023-06-27 08:46:58 +0100

    apiapp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+const App = () => {
+  const [imageUrl, setImageUrl] = useState('');
+
+  const fetchHandler = async () => {
+try{
+
+    let response = await fetch(
+      'https://api.thedogapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1'
+    );
+    console.log(response)
+    if(!response.ok){
+      throw new Error ('something wrong')
+    }
+    
+    let imageData = await response.json();
+    setImageUrl(imageData[0].url);
+    
+    
+  } catch (error){
+  console.log(error)
+    }
+  };
+
+  useEffect(() => {
+    fetchHandler();
+  }, []);
+
+  if (!imageUrl) {
+    return (
+      <>
+        <h1>Loading...</h1>
+        <button onClick={fetchHandler}>Submit</button>
+      </>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Random Dogs</h1>
+      <img src={imageUrl} alt="Random Dog" />
+      <button onClick={fetchHandler}>Submit</button>
     </div>
   );
-}
+};
 
 export default App;
